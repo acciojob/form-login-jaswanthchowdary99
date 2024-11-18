d7af1959ae64a23db4d72cef9e4b9e4ef5f4342a
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-18 14:46:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
-
+	event.preventDefault();
+	const form = document.getElementById("form1");
+	const fname = form.elements["fname"].value; 
+	const lname	=form.elements["lname"].value;
+	alert(`${fname}`+` ${lname}`);
 }
